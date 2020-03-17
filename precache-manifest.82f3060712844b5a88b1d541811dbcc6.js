21de1c460740aab619a3b90d68a93996cc5edde1
Klaudia <k.polan95@gmail.com>
2020-03-17 23:34:39 +0100

    Updates

---
+++ precache-manifest.82f3060712844b5a88b1d541811dbcc6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22e7c6ec745cd4dedbc06396839ef9fd",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "f6df6a29e8dbf197292a",
+    "url": "/tic-tac-toe/static/css/main.79c45796.chunk.css"
+  },
+  {
+    "revision": "54a6104b5e62598051c6",
+    "url": "/tic-tac-toe/static/js/2.7546a9cb.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/tic-tac-toe/static/js/2.7546a9cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6df6a29e8dbf197292a",
+    "url": "/tic-tac-toe/static/js/main.836be0d5.chunk.js"
+  },
+  {
+    "revision": "b83c3f5dfddd51f16016",
+    "url": "/tic-tac-toe/static/js/runtime-main.e9088102.js"
+  },
+  {
+    "revision": "1ca72c2e906989d3d666078f72d618a2",
+    "url": "/tic-tac-toe/static/media/pic1.1ca72c2e.jpg"
+  }
+]);
